267bd6f174bc2b9e424274020d5bf6c307d720a7
Raghu Saxena <poiasdpoiasd@live.com>
2022-03-02 21:01:22 +0800

    Fix logic

--- src/byte_functions/mod.rs
+++ src/byte_functions/mod.rs
@@ -8,17 +8,17 @@ pub fn do_nothing() {
 
 pub fn url_encoded_to_hex(urlenc: &str) -> String {
     let mut hex_str = String::new();
+    let mut i = 0;
 
-    for i in 0..urlenc.chars().count() {
-
-        let the_char = urlenc.chars().nth(i).expect("Shits fucked up yo");
-
-        
+    while i < urlenc.chars().count() {
+        let the_char = urlenc.chars().nth(i).expect("Shits fucked up yo");       
 
         if the_char == '%' {
             hex_str.push_str(&urlenc[i+1..i+3]);
+            i += 3;
         } else {
             hex_str.push_str(&hex::encode(the_char.to_string()));
+            i += 1;
         }
     }
 
--- src/byte_functions/tests.rs
+++ src/byte_functions/tests.rs
@@ -1,4 +1,11 @@
 #[test]
 fn is_legit(){
-    assert_eq!("41", super::url_encoded_to_hex("A"))
+    assert_eq!("41", super::url_encoded_to_hex("A"));
+    assert_eq!("41", super::url_encoded_to_hex("%41"));
+    assert_eq!("4141", super::url_encoded_to_hex("A%41"));
+    assert_eq!("4141", super::url_encoded_to_hex("%41A"));
+    assert_eq!("4142", super::url_encoded_to_hex("%41B"));
+    assert_eq!("4241", super::url_encoded_to_hex("B%41"));
+    assert_eq!("4241", super::url_encoded_to_hex("BA"));
+    assert_eq!("4142", super::url_encoded_to_hex("%41%42"));
 }
