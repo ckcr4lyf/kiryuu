1874c70564910dd04f0a26f6de26210b1e53a0be
Raghu Saxena <poiasdpoiasd@live.com>
2022-03-10 00:05:14 +0800

    Use raw request :gem:

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,21 @@
 mod byte_functions;
 
-use actix_web::{get, App, HttpServer, Responder, web};
+use actix_web::{get, App, HttpServer, Responder, web, HttpRequest};
 use serde::Deserialize;
 
 #[derive(Debug, Deserialize)]
 pub struct AnnounceRequest {
     infohash: String,
+    port: u16,
 }
 
 #[get("/healthz")]
-async fn healthz() -> impl Responder {
+async fn healthz(req: HttpRequest) -> impl Responder {
+    let query = req.query_string();
+    println!("Query string is {}", query);
+    let conn_info = req.connection_info();
+    let user_ip = conn_info.peer_addr().expect("Missing IP bruv");
+    println!("Bruh {:?}", user_ip);
     return "";
 }
 
@@ -17,6 +23,7 @@ async fn healthz() -> impl Responder {
 async fn announce(params: web::Query<AnnounceRequest>) -> impl Responder {
     byte_functions::do_nothing();
     println!("Got the following params: {:?}", byte_functions::url_encoded_to_hex(&params.infohash));
+    println!("Port reported as {}", params.port);
     return "GG";
 }
 
