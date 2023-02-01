3d392ecaddd5aab88d77e1f483d864ecb080b058
Raghu Saxena <poiasdpoiasd@live.com>
2023-02-01 22:28:07 +0800

    Record announces with no change

--- src/constants/mod.rs
+++ src/constants/mod.rs
@@ -1,3 +1,4 @@
 pub const ANNOUNCE_COUNT_KEY: &str = "kiryuu_http_announce_count";
+pub const NOCHANGE_ANNOUNCE_COUNT_KEY: &str = "kiryuu_http_nochange_announce_count"; // If no change to seeder_count / leecher_count
 pub const REQ_DURATION_KEY: &str = "kiryuu_http_req_seconds_sum";
 pub const TORRENTS_KEY: &str = "TORRENTS";
