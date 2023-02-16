22948629da453423a47963b1cb4bfd9e01c601e1
Raghu Saxena <poiasdpoiasd@live.com>
2023-02-16 12:14:58 +0800

    Update match

--- src/main.rs
+++ src/main.rs
@@ -67,7 +67,10 @@ async fn announce(req: HttpRequest, data: web::Data<AppState>) -> HttpResponse {
         Err(e) => match e {
             query::QueryError::ParseFailure => {
                 return HttpResponse::build(StatusCode::BAD_REQUEST).body("Failed to parse announce\n");
-            }
+            },
+            query::QueryError::InvalidInfohash => {
+                return HttpResponse::build(StatusCode::BAD_REQUEST).body("Infohash is not 40 bytes\n");
+            },
             query::QueryError::Custom(e) => {
                 return HttpResponse::build(StatusCode::BAD_REQUEST).body(e + "\n");
             }
