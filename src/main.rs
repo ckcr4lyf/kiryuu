061fdb297560d0c464dbf6c78a64a937c2f3905e
Raghu Saxena <poiasdpoiasd@live.com>
2024-05-29 17:27:17 +0800

    Some trace logging

--- src/main.rs
+++ src/main.rs
@@ -96,9 +96,11 @@ async fn announce(req: HttpRequest, data: web::Data<AppState>) -> HttpResponse {
         Ok(legit) => legit, // Just set `parsed` , let handler continue
         Err(e) => match e {
             query::QueryError::ParseFailure => {
+                trace_log!("failed to parse announce");
                 return HttpResponse::build(StatusCode::BAD_REQUEST).body("Failed to parse announce\n");
             },
             query::QueryError::InvalidInfohash => {
+                trace_log!("invalid infohash");
                 return HttpResponse::build(StatusCode::BAD_REQUEST).body("Infohash is not 20 bytes\n");
             }
         }
@@ -113,7 +115,7 @@ async fn announce(req: HttpRequest, data: web::Data<AppState>) -> HttpResponse {
     .cmd("ZSCORE").arg(&leechers_key).arg(&parsed.ip_port)
     .cmd("GET").arg(&cache_key);
     
-    let (is_seeder_v2, is_leecher_v2, cached_reply) : (Exists, Exists, Vec<u8>) = trace_wrap_v2!(pp.query_async(&mut rc).await, "redis").unwrap();
+    let (is_seeder_v2, is_leecher_v2, cached_reply) : (Exists, Exists, Vec<u8>) = trace_wrap_v2!(pp.query_async(&mut rc).await, "redis", "seeder_leecher_cache").unwrap();
 
     let mut post_announce_pipeline = redis::pipe();
     post_announce_pipeline.cmd("ZADD").arg(constants::TORRENTS_KEY).arg(time_now_ms).arg(&parsed.info_hash).ignore(); // To "update" the torrent
@@ -167,11 +169,12 @@ async fn announce(req: HttpRequest, data: web::Data<AppState>) -> HttpResponse {
     let final_res = match cached_reply.len() {
         0 => {
             // Cache miss. Lookup from redis
+            trace_log!("cache miss");
             let mut p = redis::pipe();
             let pp = p.cmd("ZRANGEBYSCORE").arg(&seeders_key).arg(max_limit).arg(time_now_ms).arg("LIMIT").arg(0).arg(50)
             .cmd("ZRANGEBYSCORE").arg(&leechers_key).arg(max_limit).arg(time_now_ms).arg("LIMIT").arg(0).arg(50);
 
-            let (seeders, leechers) : (Vec<Vec<u8>>, Vec<Vec<u8>>) = trace_wrap_v2!(pp.query_async(&mut rc).await, "redis").unwrap();
+            let (seeders, leechers) : (Vec<Vec<u8>>, Vec<Vec<u8>>) = trace_wrap_v2!(pp.query_async(&mut rc).await, "redis", "seeders_leechers").unwrap();
         
             // endex = end index XD. seems in rust cannot select first 50 elements, or limit to less if vector doesnt have 50
             // e.g. &seeders[0..50] is panicking when seeders len is < 50. Oh well.
@@ -181,6 +184,7 @@ async fn announce(req: HttpRequest, data: web::Data<AppState>) -> HttpResponse {
             query::announce_reply(seeders.len() as i64 + seed_count_mod, leechers.len() as i64 + leech_count_mod, &seeders[0..seeder_endex], &leechers[0..leecher_endex])
         },
         _ => {
+            trace_log!("cache hit");
             post_announce_pipeline.cmd("INCR").arg(constants::CACHE_HIT_ANNOUNCE_COUNT_KEY).ignore();
             cached_reply
         }
@@ -203,6 +207,7 @@ async fn announce(req: HttpRequest, data: web::Data<AppState>) -> HttpResponse {
 
         // TODO: Patch cached reply with the count mods?
         // Also invalidate existing cache
+        trace_log!("need to invalidate cache");
         post_announce_pipeline.cmd("DEL").arg(&cache_key).ignore();
     } else {
         post_announce_pipeline.cmd("INCR").arg(constants::NOCHANGE_ANNOUNCE_COUNT_KEY).ignore();
@@ -253,7 +258,7 @@ async fn announce(req: HttpRequest, data: web::Data<AppState>) -> HttpResponse {
 async fn healthz(data: web::Data<AppState>) -> HttpResponse {
     let mut rc = data.redis_connection.clone();
 
-    match trace_wrap_v2!(redis::cmd("PING").query_async::<_, ()>(&mut rc).await, "redis-hc") {
+    match trace_wrap_v2!(redis::cmd("PING").query_async::<_, ()>(&mut rc).await, "redis", "healthcheck") {
         Ok(_) => HttpResponse::build(StatusCode::OK).append_header(header::ContentType::plaintext()).body("OK"),
         Err(_) => HttpResponse::build(StatusCode::INTERNAL_SERVER_ERROR).append_header(header::ContentType::plaintext()).body("OOF"),
     }
