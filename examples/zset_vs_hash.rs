115860f76ed90260a581a0edd1bc06bf8cd31dd1
Raghu Saxena <poiasdpoiasd@live.com>
2024-12-19 23:16:09 +0800

    Working on ZSET vs HASH benchmark

--- examples/zset_vs_hash.rs
+++ examples/zset_vs_hash.rs
@@ -1,3 +1,8 @@
+use std::time::{SystemTime, UNIX_EPOCH};
+
+use rand::Rng;
+use redis::Commands;
+
 /**
  * Tryingt to find at how many members is a ZSET less efficient than a HASH, WITH HEXPIRE.
  */
@@ -10,8 +15,16 @@ fn main(){
     const HASH_KEY: &[u8; 10] = b"HASHONLYAA";
     const HASH_HEXPIRE_KEY: &[u8; 10] = b"HASHEXPIRE";
 
-    // rand::thread_rng().gen();
+    let time_now = SystemTime::now().duration_since(UNIX_EPOCH).expect("UHOH");
+    let time_now_ms: i64 = i64::try_from(time_now.as_secs()).expect("UHOH");
 
+    const TWENTY_FOUR_HOURS: i64 = 60 * 60 * 24;
+
+    // rand::thread_rng().gen();
+    for i in 0..200 {
+        let fake_infohash: [u8; 20] = rand::thread_rng().gen();
+        let () = r.zadd(ZSET_KEY, &fake_infohash, time_now_ms + TWENTY_FOUR_HOURS).expect("Failed to ZADD");
+    }
     // TODO
 
 
